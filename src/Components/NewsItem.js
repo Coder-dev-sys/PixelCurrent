0955af57c914ee29f953863ec6aa60b1342ed34e
Coder-dev-sys <dvvyas652@gmail.com>
2025-04-05 21:58:45 +0530

    Added News using api fetching

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://www.usine-digitale.fr/mediatheque/3/9/2/001525293_896x598_c.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
